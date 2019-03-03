f63ece6255fdd30da2af343f48ca8ddc82ff54b8
patrick <biggreyhairboy@gmail.com>
2019-03-03 10:12:46 +0800

    line break

--- TickDailyCheck.py
+++ TickDailyCheck.py
@@ -1,5 +1,5 @@
-from pymongo import MongoClient
 import json
+from pymongo import MongoClient
 from datetime import datetime, date, time
 
 
@@ -23,20 +23,23 @@ afternoon_end = time(15, 0)
 # count section
 night_count = 14400
 morning_count = 16200
-afternoon_count = 14400
+afternoon_count = 10800
 
 
 def runcheck():
     client = MongoClient(MONGO_HOST, MONGO_PORT)
     tick_db = client['VnTrader_Tick_Db']
     tick_collection = tick_db[symbol]
-    timedict = {"night": [night_start, night_end, night_count], "morning": [morning_start, morning_end, morning_count], "afternoon": [afternoon_start, afternoon_end, afternoon_count]}
+    timedict = {"night": [night_start, night_end, night_count], \
+                "morning": [morning_start, morning_end, morning_count],\
+                "afternoon": [afternoon_start, afternoon_end, afternoon_count]}
     for timekey in timedict.keys():
         tstart = datetime.combine(trading_date, timedict[timekey][0])
         tend = datetime.combine(trading_date, timedict[timekey][1])
         # print(tstart)
         query_condition = {'datetime': {'$gte': tstart, '$lte': tend}}
         sessioncount = tick_collection.find(query_condition).count()
+        print(sessioncount)
         if timedict[timekey][2] == sessioncount:
             print(timekey + " complete!")
         elif timedict[timekey][2] - 20 < sessioncount:
